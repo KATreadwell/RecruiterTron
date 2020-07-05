07833cae342cca2654b4408570e15ef4e03ba784
KAT <katreadwell1@gmail.com>
2020-07-04 23:35:16 -0700

    added calendar and table components from template

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -1,4 +1,7 @@
 import Dashboard from '../views/dashboard/dashboard';
+import CandidateDatatable from '../views/tables/candidate-table';
+import PositionDatatable from '../views/tables/position-table';
+import Calendar from '../views/calendar/calendar';
 
 var ThemeRoutes = [
 	{
@@ -10,8 +13,42 @@ var ThemeRoutes = [
 		path: '/dashboard',
 		name: 'Dashboard',
 		icon: 'ti-dashboard',
-		component: Dashboard
+		child: [{
+            path: "/dashboards/classic",
+            name: "Classic",
+            mini: "B",
+            icon: "mdi mdi-adjust",
+            component: Dashboard
+		}]
 	},
+	{
+        navlabel: true,
+        name: "Tables",
+        icon: "mdi mdi-dots-horizontal",
+	},
+	{
+        path: '/tables/candidate',
+        name: 'Candidate',
+        icon: 'mdi mdi-border-inside',
+        component: CandidateDatatable
+	},
+	{
+        path: '/tables/position',
+        name: 'Position',
+        icon: 'mdi mdi-border-inside',
+        component: PositionDatatable
+	},
+	{
+        navlabel: true,
+        name: "Calendar",
+        icon: "mdi mdi-dots-horizontal",
+	},
+	{
+        path: '/calendar',
+        name: 'Calendar',
+        icon: 'mdi mdi-calendar',
+        component: Calendar
+    },
 	{ path: '/', pathTo: '/dashboard', name: 'Dashboard', redirect: true }
 ];
 export default ThemeRoutes;
