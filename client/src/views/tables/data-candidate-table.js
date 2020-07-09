bc1d178ea0762a389cf96b371c10ab76cdcc02b3
KAT <katreadwell1@gmail.com>
2020-07-08 21:30:58 -0700

    committing changes made with diarmuid around UI, got address to show properly on candidate table

--- client/src/views/tables/data-candidate-table.js
+++ client/src/views/tables/data-candidate-table.js
@@ -1,4 +1,4 @@
-const jsondataCandidate = [
+let jsondataCandidate = [
     {
         "name": "Batman",
         "phone": "310-222-2222",
@@ -10,7 +10,18 @@ const jsondataCandidate = [
         "experience": "5+ years",
         "qualifications": ["Firearms Permit", "Guard Card", "Prior Law Enforcement Experience", "Transportation", "Driver's License"],
         "commute": "10"
-    }   
+    }
 ]
 
+jsondataCandidate[0].address = `${jsondataCandidate[0].street}, ${jsondataCandidate[0].city}, ${jsondataCandidate[0].state}, ${jsondataCandidate[0].zip}`;
+
+console.log(jsondataCandidate);
+
+// const addressCandidate = [
+//     {
+//         "address": `${jsondataCandidate[0].street}, ${jsondataCandidate[0].city}, ${jsondataCandidate[0].state}, ${jsondataCandidate[0].zip}`
+//     }
+// ]
+
 export { jsondataCandidate };
+// export { addressCandidate };
--- models/Candidate.js
+++ models/Candidate.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CandidateSchema = new Schema({
     name: { type: String, required: true}, 
-    phone: {type: Number, required: true},
+    phone: {type: String, required: true},
     email: String,
     street: String,
     city: String,
