a886c9b327c2fad0bd08d4f4bb0683812507e9b5
KAT <katreadwell1@gmail.com>
2020-07-15 21:04:13 -0700

    well....got a mock-up of google maps, it looks terrible!

--- client/src/views/google/google.js
+++ client/src/views/google/google.js
@@ -4,15 +4,68 @@ import {
     CardBody,
 
 } from 'reactstrap';
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const Google = () => {
-    return (
-        <Card>
-            <CardBody>
-                Start your project from here...
-				</CardBody>
-        </Card>
-    );
-}
+// const Google = () => {
+//     return (
+//         <Card>
+//             <CardBody>
+            
 
-export default Google;
+//             </CardBody>
+//         </Card>
+//     );
+// }
+
+
+
+const Google = compose(
+    withProps({
+      googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `400px` }} />,
+      mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+  )((props) =>
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    </GoogleMap>
+  )
+  
+  class MyFancyComponent extends React.PureComponent {
+    state = {
+      isMarkerShown: false,
+    }
+  
+    componentDidMount() {
+      this.delayedShowMarker()
+    }
+  
+    delayedShowMarker = () => {
+      setTimeout(() => {
+        this.setState({ isMarkerShown: true })
+      }, 3000)
+    }
+  
+    handleMarkerClick = () => {
+      this.setState({ isMarkerShown: false })
+      this.delayedShowMarker()
+    }
+  
+    render() {
+      return (
+        <Google
+          isMarkerShown={this.state.isMarkerShown}
+          onMarkerClick={this.handleMarkerClick}
+        />
+      )
+    }
+  }
+
+  export default Google;
